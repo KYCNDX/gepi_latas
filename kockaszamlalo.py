b23614618403e1f1e7e0d3e1e11579115d1e059d
KYCNDX <92981479+KYCNDX@users.noreply.github.com>
2021-12-14 11:05:52 +0100

    Update kockaszamlalo.py

--- kockaszamlalo.py
+++ kockaszamlalo.py
@@ -1,20 +1,57 @@
 import cv2
-import numpy as np
+import numpy as np;
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+# kep beolvasasa
+im = cv2.imread("./kepek/20211210_111225.jpg", 0)
 
-if vc.isOpened(): #kepteszteles
-    rval, frame = vc.read()
-else:
-    rval = False
+#kep atmeretezese
+down_width = 800
+down_height = 600
+down_points = (down_width, down_height)
+resized_down = cv2.resize(im, down_points, interpolation= cv2.INTER_LINEAR)
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: #ESC-re kilepes
-        break
+#threshold beallitas
+th, dst = cv2.threshold(resized_down, 127, 255, cv2.THRESH_BINARY);
+cv2.imshow("Treshold", dst)
+cv2.imwrite("treshold.jpg", dst);
 
-vc.release()
-cv2.destroyWindow("preview")
+#pont kereso parameterei
+params = cv2.SimpleBlobDetector_Params()
+
+#treshold beallitasok
+params.minThreshold = 127
+params.maxThreshold = 255
+
+
+# teruleti szuro
+params.filterByArea = True
+params.minArea = 15
+
+# kereksegi szuro
+params.filterByCircularity = True
+params.minCircularity = 0.1
+
+# forma kereksegi szuro
+params.filterByConvexity = True
+params.minConvexity = 0.47
+
+# elipszis forma szuro
+params.filterByInertia = True
+params.minInertiaRatio = 0.1
+
+# erzekelo letrehozasa a marameterekkel
+detector = cv2.SimpleBlobDetector_create(params)
+
+# pontok kereses
+keypoints = detector.detect(dst)
+
+# pontok bejelolese pirossal
+
+im_with_keypoints = cv2.drawKeypoints(dst, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# megmutatas
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
+
+#ablak bezarasa
+cv2.destroyAllWindows()
